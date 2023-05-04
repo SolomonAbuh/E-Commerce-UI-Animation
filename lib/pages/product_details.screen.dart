11eef4f3c5f0f3bd0f854de17ae67ac4459fed53
SOLOMON ABUH <soloabuh2@gmail.com>
2023-05-04 17:44:29 +0100

    update on product_details.screen.dart 🛠

--- lib/pages/product_details.screen.dart
+++ lib/pages/product_details.screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:money_point_take_home_assignment/constants/app_assets.dart';
 
 import 'package:money_point_take_home_assignment/models/product_model.dart';
+import 'package:money_point_take_home_assignment/widgets/app_spacer.dart';
+import 'package:sizer/sizer.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   final ProductModel model;
@@ -14,11 +18,29 @@ class ProductDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        iconTheme: ,
+        iconTheme: const IconThemeData(color: Colors.black),
+        elevation: 0,
+        actions: [
+          const Icon(Icons.favorite_outline_rounded),
+          spaceH(4.w),
+          const Icon(Icons.share),
+          spaceH(4.w),
+          SvgPicture.asset(AppAssets.shoppingBagIcon),
+          spaceH(4.w),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [Image.asset(model.images[0])],
+          children: [
+            Center(
+              child: SizedBox(
+                height: 40.h,
+                child: Image.asset(
+                  model.images[0],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
