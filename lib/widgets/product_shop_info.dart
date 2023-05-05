8afbaec5b430753058d32be6044acd3dc54c0df5
SOLOMON ABUH <soloabuh2@gmail.com>
2023-05-05 23:49:59 +0100

    update on product_shop_info.dart 🛠

--- lib/widgets/product_shop_info.dart
+++ lib/widgets/product_shop_info.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:money_point_take_home_assignment/constants/app_assets.dart';
+import 'package:money_point_take_home_assignment/constants/colors.dart';
+import 'package:sizer/sizer.dart';
 
 class ProductShopInfo extends StatelessWidget {
   const ProductShopInfo({super.key});
@@ -10,8 +13,9 @@ class ProductShopInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        CircleAvatar(
-          backgroundImage:Image AppAssets.dsquaredJacket,
+        SizedBox(
+          height: 10.h,
+          child: SvgPicture.asset(AppAssets.navyBlueJacketThree),
         )
       ],
     );
